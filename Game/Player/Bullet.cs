a9ff269951b5ea840e752c73b48affb041d7dd73
Mandeep <Mandeepsinghjass@gmail.com>
2024-03-30 09:45:05 +0530

    Added Destroy feature for bullets so that whenever they go off screen we destroy the gameObject

--- Game/Player/Bullet.cs
+++ Game/Player/Bullet.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private Camera _camera;
+
+    private void Awake(){
+        _camera = _camera.main;
+    }
+    private void Update(){
+        DestroyWhenOffScreen();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<EnemyMovement>())
         {
 
             Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+    private void DestroyWhenOffScreen(){
+        Vector2 screenPosition = _camera.WorldToScreenPoint(transform.position);
+        if(screenPosition.x < 0 || screenPosition.x > _camera.pixelWidth || screenPosition.y < 0 || screenPosition.y > _camera.pixelHeight){
             Destroy(gameObject);
         }
     }
