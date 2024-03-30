a78e6570b8214e1589fe12505562d41766d7cf10
Mandeep <Mandeepsinghjass@gmail.com>
2024-03-30 09:38:36 +0530

    Added Screen border for enemy in enemy Movement script

--- Game/Enemy/EnemyMovement.cs
+++ Game/Enemy/EnemyMovement.cs
@@ -5,59 +5,100 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField]
-    private  float _speed ;
+    private float _speed;
     [SerializeField]
     private float _rotationSpeed;
+    [SerializeField]
+    private float _screenBorder;
     private Rigidbody2D _rigidbody;
 
     private PlayerAwarenessController _playerAwarenessController;
     private Vector2 _targetDirection;
     private float _changeDirectionCooldown;
 
-    private void Awake(){
+    private Camera _camera;
+
+    private void Awake()
+    {
         _rigidbody = GetComponent<Rigidbody2D>();
         _playerAwarenessController = GetComponent<PlayerAwarenessController>();
         _targetDirection = transform.up;
+        _camera = Camera.main;
     }
-       void FixedUpdate()
+    void FixedUpdate()
     {
         UpdateTargetDirection();
         RotateTowardsTarget();
         SetVelocity();
     }
-    private void UpdateTargetDirection(){
+    private void UpdateTargetDirection()
+    {
         HandleRandomDirectionChange();
         HandlePlayerTargeting();
+        HandleEnemyOffScreen();
 
     }
 
-    private void HandleRandomDirectionChange(){
-        _changeDirectionCooldown -=  Time.deltaTime;
-        if (_changeDirectionCooldown<=0)
+    private void HandleRandomDirectionChange()
+    {
+        _changeDirectionCooldown -= Time.deltaTime;
+        if (_changeDirectionCooldown <= 0)
         {
-            float angleChange = Random.Range(-90f,90f);
-            Quaternion rotation = Quaternion.AngleAxis(angleChange , transform.forward);
+            float angleChange = Random.Range(-90f, 90f);
+            Quaternion rotation = Quaternion.AngleAxis(angleChange, transform.forward);
             _targetDirection = rotation * _targetDirection;
-            _changeDirectionCooldown = Random.Range(1f,5f);
+            _changeDirectionCooldown = Random.Range(1f, 5f);
         }
     }
-    private void HandlePlayerTargeting(){
-if(_playerAwarenessController.AwareOfPlayer == true){
+    private void HandlePlayerTargeting()
+    {
+        if (_playerAwarenessController.AwareOfPlayer == true)
+        {
             _targetDirection = _playerAwarenessController.DirectionToPlayer;
         }
     }
 
+    // private void HandleEnemyOffScreen(){
+    // Vector2 screenPosition = _camera.WorldToScreenPoint(transform.position);
 
-    private void RotateTowardsTarget(){
-        Quaternion targetRotation = Quaternion.LookRotation(transform.forward , _targetDirection);
-        Quaternion rotation = Quaternion.RotateTowards(transform.rotation , targetRotation , _rotationSpeed * Time.deltaTime);
+    // if ((screenPosition.x <_screenBorder && _targetDirection.x <0)|| (screenPosition.x > _camera.pixelWidth - _screenBorder && _targetDirection.x >0))
+    // {
+    //     _targetDirection = new Vector2(-_targetDirection , _targetDirection.y);
+    // }
+    // if ((screenPosition.y < _screenBorder && _targetDirection.y <0)|| (screenPosition.y > _camera.pixelHeight - _screenBorder && _targetDirection.y >0))
+    // {
+    //     _targetDirection = new Vector2(_targetDirection.x, -_targetDirection );
+    // }
+    // }
+
+    private void HandleEnemyOffScreen()
+    {
+        Vector2 screenPosition = _camera.WorldToScreenPoint(transform.position);
+
+        if ((screenPosition.x < _screenBorder && _targetDirection.x < 0) || (screenPosition.x > _camera.pixelWidth - _screenBorder && _targetDirection.x > 0))
+        {
+            _targetDirection.x = -_targetDirection.x;
+        }
+
+        if ((screenPosition.y < _screenBorder && _targetDirection.y < 0) || (screenPosition.y > _camera.pixelHeight - _screenBorder && _targetDirection.y > 0))
+        {
+            _targetDirection.y = -_targetDirection.y;
+        }
+    }
+
+
+    private void RotateTowardsTarget()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _targetDirection);
+        Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
 
         _rigidbody.SetRotation(rotation);
     }
-    private void SetVelocity(){
-        
-            _rigidbody.velocity = transform.up * _speed;
-        }
-    
+    private void SetVelocity()
+    {
+
+        _rigidbody.velocity = transform.up * _speed;
+    }
+
 
 }
