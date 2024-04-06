26944c3b12287b0b51638c97117b675c30898f82
Mandeep <Mandeepsinghjass@gmail.com>
2024-04-06 15:42:40 +0530

    Removed all the bugs and added the functionality for enemyspawner using enemySpawner script

---
+++ Game/Enemy/EnemySpawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+[SerializeField]
+    private float _minimumSpawnTime;
+    [SerializeField]
+    private float _maximumSpawnTime;
+    private float _timeUntilSpawn;
+
+
+
+    void Awake()
+    {
+        SetTimeUntilSpawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timeUntilSpawn -= Time.deltaTime;
+        if (_timeUntilSpawn <= 0 )
+        {
+            Instantiate(_enemyPrefab,transform.position , Quaternion.identity);
+            SetTimeUntilSpawn();
+        }
+    }
+
+    private void SetTimeUntilSpawn(){
+        _timeUntilSpawn = Random.Range(_minimumSpawnTime,_maximumSpawnTime);
+    }
+}
